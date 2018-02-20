6b0432aa94bd1676da976e07e8205f2ac0ea8b86
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-02-19 21:29:31 -0700

    [routes] add routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const { check } = require('express-validator/check');
+
+const MissyCoin = require('../middleware/MissyCoin');
+
+const responseMiddleware = (req, res, next) => {
+  return res.json(req.responseValue);
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'MissyCoin' });
 });
+
+router.post('/transactions/new', [
+  check('sender', 'Sender must be a String').exists(),
+  check('recipient', 'recipient must be a String').exists(),
+  check('amount', 'Sender must be an Int Value').isInt().exists()
+], MissyCoin.newTransaction, responseMiddleware)
+
+router.get('/mine', MissyCoin.mine, responseMiddleware)
+
+router.get('/chain', MissyCoin.getChain, responseMiddleware)
 
 module.exports = router;
